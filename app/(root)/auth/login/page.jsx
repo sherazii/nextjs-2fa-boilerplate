cc38a42216e5e4146f98de6f784d12b848bea027
SherazHashmi1111 <sherazhashmi111@gmail.com>
2025-10-07 13:36:22 +0500

    Fonts added in global css and login component is started to create

--- app/(root)/auth/login/page.jsx
+++ app/(root)/auth/login/page.jsx
@@ -1,29 +1,37 @@
 import {
   Card,
-  CardAction,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-
+} from "@/components/ui/card";
+import Logo from "@/public/assets/images/logo-black.png";
+import Image from "next/image";
 const LoginPage = () => {
   return (
     <Card>
-  <CardHeader>
-    <CardTitle>Card Title</CardTitle>
-    <CardDescription>Card Description</CardDescription>
-    <CardAction>Card Action</CardAction>
-  </CardHeader>
-  <CardContent>
-    <p>Card Content</p>
-  </CardContent>
-  <CardFooter>
-    <p>Card Footer</p>
-  </CardFooter>
-</Card>
-  )
-}
+      <CardHeader>
+        <div className="flex justify-center">
+          <Image
+            src={Logo}
+            className="max-w-[150px] h-auto"
+            alt="logoImg"
+            priority
+          />
+        </div>
+        <CardTitle className="w-full text-center text-3xl font-bold font-[Pacifico] text-orange-500">
+          Login Into Account
+        </CardTitle>
+        <p>Login into your account by filling out the form below.</p>
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+      <CardFooter>
+        <p>Card Footer</p>
+      </CardFooter>
+    </Card>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
--- app/layout.jsx
+++ app/layout.jsx
@@ -8,7 +8,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={` antialiased`} suppressHydrationWarning={true}>
+      <body
+        className={` antialiased font-[M_PLUS_Rounded]`}
+        suppressHydrationWarning={true}
+      >
         {children}
       </body>
     </html>
