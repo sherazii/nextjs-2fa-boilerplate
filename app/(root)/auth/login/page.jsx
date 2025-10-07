3c90724559c26e65984dc4f061bd7ea9c35ade59
SherazHashmi1111 <sherazhashmi111@gmail.com>
2025-10-07 13:22:43 +0500

    Added card from shadcn

--- app/(root)/auth/login/page.jsx
+++ app/(root)/auth/login/page.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
-const Login = () => {
+const LoginPage = () => {
   return (
-    <div>Login</div>
+    <Card>
+  <CardHeader>
+    <CardTitle>Card Title</CardTitle>
+    <CardDescription>Card Description</CardDescription>
+    <CardAction>Card Action</CardAction>
+  </CardHeader>
+  <CardContent>
+    <p>Card Content</p>
+  </CardContent>
+  <CardFooter>
+    <p>Card Footer</p>
+  </CardFooter>
+</Card>
   )
 }
 
-export default Login
+export default LoginPage
