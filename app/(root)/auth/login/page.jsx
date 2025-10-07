cb94cce0c34df277a8a10147717e65ed265b4bea
SherazHashmi1111 <sherazhashmi111@gmail.com>
2025-10-07 13:44:35 +0500

    Install zod and create zodSchema

--- app/(root)/auth/login/page.jsx
+++ app/(root)/auth/login/page.jsx
@@ -5,8 +5,11 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import Logo from "@/public/assets/images/logo-black.png";
 import Image from "next/image";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 const LoginPage = () => {
   return (
     <Card>
@@ -25,11 +28,52 @@ const LoginPage = () => {
         <p>Login into your account by filling out the form below.</p>
       </CardHeader>
       <CardContent>
-        <p>Card Content</p>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(LoginSubmitHandler)}
+            className="space-y-8"
+          >
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter Your Email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem className="relative">
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="************"
+                      type={isTypePassword ? "password" : "text"}
+                      {...field}
+                    />
+                  </FormControl>
+                  <button
+                    type="button"
+                    className="absolute right-4 top-[38px] cursor-pointer"
+                    onClick={() => setIsTypePassword(!isTypePassword)}
+                  >
+                    {isTypePassword ? <FaEyeSlash /> : <FaEye />}
+                  </button>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
       </CardContent>
-      <CardFooter>
-        <p>Card Footer</p>
-      </CardFooter>
     </Card>
   );
 };
